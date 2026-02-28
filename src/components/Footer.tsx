42f1abffa1dc444389deacf303b48552d762f808
Kian O'Connor <kianoconnor@MacBook-Pro.local>
2026-02-27 16:45:18 -0800

    Add OpenSubsidies to sister site links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -72,6 +72,7 @@ export default function Footer() {
               <li><a href="https://www.openimmigration.us" className="text-sm hover:text-white transition-colors">OpenImmigration</a></li>
               <li><a href="https://www.openspending.us" className="text-sm hover:text-white transition-colors">OpenSpending</a></li>
               <li><a href="https://www.vaccinewatch.org" className="text-sm hover:text-white transition-colors">VaccineWatch</a></li>
+              <li><a href="https://www.opensubsidies.org" className="text-sm hover:text-white transition-colors">OpenSubsidies</a></li>
               <li><a href="https://thedataproject.ai" className="text-sm hover:text-white transition-colors">TheDataProject.ai</a></li>
             </ul>
           </div>
