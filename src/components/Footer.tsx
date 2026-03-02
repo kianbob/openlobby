2ab84aa413f03f15f51f3af9d65d847e42977c19
Kian O'Connor <kianoconnor@MacBook-Pro.local>
2026-03-02 15:47:08 -0800

    add OpenPrescriber to sister sites

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -74,6 +74,7 @@ export default function Footer() {
               <li><a href="https://www.vaccinewatch.org" className="text-sm hover:text-white transition-colors">VaccineWatch</a></li>
               <li><a href="https://www.opensubsidies.org" className="text-sm hover:text-white transition-colors">OpenSubsidies</a></li>
               <li><a href="https://www.warcosts.org" className="text-sm hover:text-white transition-colors">WarCosts</a></li>
+              <li><a href="https://www.openprescriber.org" className="text-sm hover:text-white transition-colors">OpenPrescriber</a></li>
               <li><a href="https://thedataproject.ai" className="text-sm hover:text-white transition-colors">TheDataProject.ai</a></li>
             </ul>
           </div>
